8f835178f22fa9cc8060e6b04761895e57b5db73
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-24 20:43:20 +0600

    Added components

---
+++ src/components/ProjectBlock/ProjectContactMeBlock.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProjectContactMeBlock = () => {
+    return (
+        <section className="py-5 bg-gradient-primary-to-secondary text-white">
+            <div className="container px-5 my-5">
+                <div className="text-center">
+                    <h2 className="display-4 fw-bolder mb-4">Let's build something together</h2>
+                    <a className="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="#">Contact
+                        me</a>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ProjectContactMeBlock;
