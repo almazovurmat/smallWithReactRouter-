c8f33c352e2166c8b8f6538956f1d0a1cf1247fa
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-24 20:42:16 +0600

    imported containers in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import MainPage from "./containers/MainPage/MainPage";
+import Projects from "./containers/Projects/Projects";
+import AboutMe from "./containers/Aboutme/AboutMe";
+import Contact from "./containers/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import './App.css';
+import Page404 from "./containers/404/page404";
 
 const App = () => (
     <div className="App">
+        <main className="flex-shrink-0">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={(
+                    <MainPage />
+                )} />
 
+                <Route path="/projects" element={(
+                    <Projects />
+                )} />
+
+                <Route path="/about-me" element={(
+                    <AboutMe />
+                )} />
+
+                <Route path="/contact" element={(
+                    <Contact />
+                )} />
+
+                <Route path="*" element={(
+                    <Page404 />
+                )} />
+            </Routes>
+        </main>
+        <Footer />
     </div>
 );
 
